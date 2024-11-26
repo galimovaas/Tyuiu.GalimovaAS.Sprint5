bb14e3525b68829b1b8756fe06c4b7bb2bd1a630
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-11-26 21:35:20 +0500

    Task6

---
+++ Tyuiu.GalimovaAS.Sprint5.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using System.IO;
+using Tyuiu.GalimovaAS.Sprint5.Task6.V21.Lib;
+namespace Tyuiu.GalimovaAS.Sprint5.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
